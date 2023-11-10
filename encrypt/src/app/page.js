55ee074a64a52ea215a1a1893a08aec659c4610b
PrakashDev <prakash.t.r.2021.cse@ritchennai.edu.in>
2023-11-10 15:48:54 +0000

    fixed bugs

--- encrypt/src/app/page.js
+++ encrypt/src/app/page.js
@@ -3,21 +3,22 @@ import React from 'react'
 import { encrypt } from 'eth-sig-util';
 
 const message = 'Hello, World!';
-const encryptionPublicKey = "+KPaILaUYGofR8o/E7f23TamdN0Xk7uVVWqcdY9bxVg=";
-const decryptionPublicKey='0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446'
+const encryptionPublicKey = "0rBPS+g/Jd7QuXoVXriM+W9oUs/E4L/ytMxSVFiD80g=";
+// const decryptionPublicKey='0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446'
 
 const encryptedMessage = encrypt(encryptionPublicKey, { data: message }, 'x25519-xsalsa20-poly1305');
+const ciphertext=`0x${Buffer.from(JSON.stringify(encryptedMessage), "utf8").toString("hex")}`;
 
-console.log('Encrypted message:', encryptedMessage);
-async function decryptedMessage(){
+console.log(`encrypted message: 0x${Buffer.from(JSON.stringify(encryptedMessage), "utf8").toString("hex")}`);
+async function decryptedMessage() {
 
   const decryptedMessages = await window.ethereum.request({
     method: 'eth_decrypt',
-    params: ["btXRswWJu2gJ6yCUaPn6B8Z5ipuMzqbMX9lUUOs=", "0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446"],
+    params: [ciphertext, "0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446"],
   });
   console.log(decryptedMessages)
 }
-async function connect(){
+async function connect() {
   await window.ethereum.request({
     "method": "eth_requestAccounts",
     "params": []
@@ -27,7 +28,7 @@ async function connect(){
 
 function page() {
   return (<>
-   
+
     <button className='bg-white text-black' onClick={decryptedMessage}>clickme</button>
     <button className='bg-white text-black' onClick={connect}>connect</button>
   </>
