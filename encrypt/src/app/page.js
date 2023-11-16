dbe1c2361638d82b1deb9f8d643e225f6adce602
PrakashDev <prakash.t.r.2021.cse@ritchennai.edu.in>
2023-11-16 12:42:35 +0000

    some random test

--- encrypt/src/app/page.js
+++ encrypt/src/app/page.js
@@ -2,27 +2,38 @@
 import React from 'react'
 import { encrypt } from 'eth-sig-util';
 
+
+
+
 const personData = {
   "name": "John Doe",
-  "dob": "1990-01-01", // Date of Birth in YYYY-MM-DD format
-  "phoneno": "+1234567890", // Placeholder phone number
-  "age": 33
-   // Placeholder age
+  "dob": "1990-01-01", 
+  "phoneno": "+1234567890", 
+  "age": 33,
+  "signature":"0x1b2f3d4a5b6c7d8e9f0a1b2c3d4e5f60708090a0b1c2d3e4f5060708090a0b1c "
 };
 
 const message = JSON.stringify(personData);
 const encryptionPublicKey = "0rBPS+g/Jd7QuXoVXriM+W9oUs/E4L/ytMxSVFiD80g=";
-// const decryptionPublicKey='0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446'
 
-const encryptedMessage = encrypt(encryptionPublicKey, { data: message }, 'x25519-xsalsa20-poly1305');
-const ciphertext=`0x${Buffer.from(JSON.stringify(encryptedMessage), "utf8").toString("hex")}`;
+async function fetchs() {
+  const details = 'https://ipfs.io/ipfs/bafkreigfvngoydofemwj5x5ioqsaqarvlprzgxinkcv3am3jpv2sysqobi';  
+    const response = await fetch(details);
 
-console.log(`encrypted message: 0x${Buffer.from(JSON.stringify(encryptedMessage), "utf8").toString("hex")}`);
-async function decryptedMessage() {
+    const datas = await response.json();
+    const encryptedMessage = encrypt(encryptionPublicKey, { data: JSON.stringify(datas)}, 'x25519-xsalsa20-poly1305');
+    const ciphertext=`0x${Buffer.from(JSON.stringify(encryptedMessage), "utf8").toString("hex")}`;
+    decryptedMessage(ciphertext)
+}
+
+
+
+
+async function decryptedMessage(text) {
 
   const decryptedMessages = await window.ethereum.request({
     method: 'eth_decrypt',
-    params: [ciphertext, "0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446"],
+    params: [text, "0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446"],
   });
   console.log(decryptedMessages)
 }
@@ -37,8 +48,9 @@ async function connect() {
 function page() {
   return (<>
 
-    <button className='bg-white text-black' onClick={decryptedMessage}>clickme</button>
+    <button className='bg-white text-black' onClick={fetchs}>clickme</button>
     <button className='bg-white text-black' onClick={connect}>connect</button>
+    <button className='bg-white text-black' onClick={decryptedMessage}>decrypt</button>
   </>
   )
 }
