d9d06c402c1edc27bfc44588f2e41b13234dc161
PrakashDev <prakash.t.r.2021.cse@ritchennai.edu.in>
2023-11-19 02:25:01 +0000

    fetching is now server sided

--- encrypt/src/app/page.js
+++ encrypt/src/app/page.js
@@ -1,37 +1,23 @@
 "use client"
 import React from 'react'
 import { encrypt } from 'eth-sig-util';
-
-
-
-
-const personData = {
-  "name": "John Doe",
-  "dob": "1990-01-01", 
-  "phoneno": "+1234567890", 
-  "age": 33,
-  "signature":"0x1b2f3d4a5b6c7d8e9f0a1b2c3d4e5f60708090a0b1c2d3e4f5060708090a0b1c "
-};
-
-const message = JSON.stringify(personData);
 const encryptionPublicKey = "0rBPS+g/Jd7QuXoVXriM+W9oUs/E4L/ytMxSVFiD80g=";
 
 async function fetchs() {
-  const details = 'https://ipfs.io/ipfs/bafkreigfvngoydofemwj5x5ioqsaqarvlprzgxinkcv3am3jpv2sysqobi';  
-    const response = await fetch(details);
-
-    const datas = await response.json();
-    const encryptedMessage = encrypt(encryptionPublicKey, { data: JSON.stringify(datas)}, 'x25519-xsalsa20-poly1305');
-    const ciphertext=`0x${Buffer.from(JSON.stringify(encryptedMessage), "utf8").toString("hex")}`;
-    decryptedMessage(ciphertext)
+   const response = await fetch('./api/ipf', {
+    method: 'GET',
+    headers: {
+      accept: 'application/json'
+    }
+  }); 
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
 }
 
-
-
-
 async function decryptedMessage(text) {
-
-  const decryptedMessages = await window.ethereum.request({
+   const decryptedMessages = await window.ethereum.request({
     method: 'eth_decrypt',
     params: [text, "0xb30585f4c2b942c28cbdf42e387ba6cb9a6da446"],
   });
@@ -43,14 +29,18 @@ async function connect() {
     "params": []
   });
 }
-
+async function getdata(){
+   const data=await fetchs();
+   const encryptedMessage = encrypt(encryptionPublicKey, { data: JSON.stringify(data)}, 'x25519-xsalsa20-poly1305');
+    const ciphertext=`0x${Buffer.from(JSON.stringify(encryptedMessage), "utf8").toString("hex")}`;
+    decryptedMessage(ciphertext)
+    console.log(data)
+ }
 
 function page() {
   return (<>
-
-    <button className='bg-white text-black' onClick={fetchs}>clickme</button>
+    <button className='bg-white text-black' onClick={getdata}>get data and decrypt</button>
     <button className='bg-white text-black' onClick={connect}>connect</button>
-    <button className='bg-white text-black' onClick={decryptedMessage}>decrypt</button>
   </>
   )
 }
